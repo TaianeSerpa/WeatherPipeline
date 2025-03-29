c137e6ffff77c4cc7a306d40930ef05f0a225d72
Taiane <taianeserpa0818@gmail.com>
2025-03-29 13:44:25 -0300

    Feat: Pipeline concluido  - Organizacao do script;  - Finalizando a parte de load no banco de dados;  - Subindo um exemple do .env para conseguir rodar o codigo;

--- Pipeline.py
+++ Pipeline.py
@@ -1,19 +1,24 @@
-# %%
+#%%
 import json 
 import os 
 import requests
 import pandas 
-from dotenv import load_dotenv
-from datetime import datetime
-import matplotlib.pyplot as plt
-import seaborn as sns
+import psycopg2
+from sqlalchemy import create_engine
 
 #Pega a chave no .env
 api_key =os.getenv('API_KEY')
 city = os.getenv('city')
 
+#Conectando ao PostgreSQL
+db_user = os.getenv('db_username')
+db_password = os.getenv('db_password')
+db_host = os.getenv('db_host')
+db_port = os.getenv('db_port')
+db_name = os.getenv('db_name')
+
 #url para trazer os dados
-url = f"http://api.weatherapi.com/v1/forecast.json?key={api_key}&q={city}&days=1&lang=pt"
+url = f"http://api.weatherapi.com/v1/forecast.json?key={api_key}&q={city}&days=5&lang=pt"
 
 #Faz a requisição e trata os dados
 response = requests.get(url)
@@ -23,10 +28,13 @@ if response.status_code == 200:
     print(json_str)
     weather_data = [ ]
 
-# %%
+#%%
 #Tratando os dados
 
-for hour in data['forecast']['forecastday'][0]['hour']:
+# Criando um dataframe com os dados
+for i in range(0, len(data['forecast']['forecastday'][0]['hour']), 3):
+        hour = data['forecast']['forecastday'][0]['hour'][i]
+
         date_time = hour["time"]# Data e hora
         temp = hour["temp_c"]# temperatura
         pressure = hour["pressure_mb"]# pressão atmosferica
@@ -39,6 +47,7 @@ for hour in data['forecast']['forecastday'][0]['hour']:
         state = data["location"] ["region"]# estado
         country = data["location"]["country"]# pais
 
+        # Adicionando dados à lista de dados
         weather_data.append({
             "Data_Hora": date_time,
             "Temperatura": temp,
@@ -54,11 +63,25 @@ for hour in data['forecast']['forecastday'][0]['hour']:
         })
 
         df = pandas.DataFrame(weather_data)
+        print(f'Dados Tratados')
         print(df)
 else:
     print(f"Erro ao pegar os dados:{response.status_code}")
-df.head()
 
-# %%
+# Mostrando as primeiras linhas do Df
+df.head(100)
 
-# %%
+#%%
+#Cerregando os dados do BD
+
+# Criando engine de conexão com PostgreSQL
+engine = create_engine(f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}')
+
+#Criar a tabela
+table_name = 'weather'
+
+# Coloca os dados no Banco de dados
+df.to_sql(table_name, engine, if_exists='replace', index = False)
+
+engine.dispose()
+
